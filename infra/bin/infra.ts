b211593198d1234ed1db4a5ca6fcf5aa4120c5ef
ritogk <your.email@example.com>
2024-08-28 19:25:15 +0900

    fix: スタック名修正

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -4,8 +4,7 @@ import * as cdk from "aws-cdk-lib";
 import { InfraStack } from "../lib/infra-stack";
 
 const app = new cdk.App();
-// TASK: InfraStack の名称を変える。
-new InfraStack(app, "InfraStack", {
+new InfraStack(app, "DockerPipelineScriptStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
